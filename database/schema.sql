4fed0f982a5b33cc7fe6a5d0cdc36535429bfdee
Clayton Lin <clin.acc.dev@gmail.com>
2018-12-03 12:51:30 -0600

    Create database schema for 'foods' table

---
+++ database/schema.sql
@@ -0,0 +1,48 @@
+-- ---
+-- HOW TO USE 
+-- PIPE THIS FILE INTO YOUR MYSQL DATABASE TO CLEAN THE DB AND TABLES
+--
+--
+
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+
+--  DROP DATABASE IF EXISTS devdb001;
+--  CREATE DATABASE devdb001;
+
+
+USE devdb001;
+
+
+-- ---
+-- Table 'foods'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS foods;
+
+CREATE TABLE foods (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  created_at TIMESTAMP NULL DEFAULT NULL,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  food VARCHAR(255) NOT NULL,
+  serving_unit VARCHAR(255) NOT NULL,
+  serving_size SMALLINT NOT NULL,
+  calories DECIMAL(18,0) NOT NULL,
+  protein DECIMAL(18,1) NOT NULL,
+  fat DECIMAL(18,1) NOT NULL,
+  carbs DECIMAL(18,1) NOT NULL,
+  total_fiber DECIMAL(18,1) NOT NULL,
+  is_estimated_macros BOOLEAN NOT NULL DEFAULT FALSE,
+  purchase_unit VARCHAR(255) NOT NULL,
+  purchase_price DECIMAL(18,2) NOT NULL,
+  cooked_percent_weight DECIMAL(18,2) NOT NULL,
+  cost_per_cooked_serving DECIMAL(18,2) NOT NULL,
+  description TEXT NULL DEFAULT NULL,
+  recipe INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+)
